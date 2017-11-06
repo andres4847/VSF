d991ac3a4583214fa6fc6449af187882f436e7de
andres <silvaandres@hotmail.com>
2017-11-06 16:20:39 -0300

    Add GTM

--- public/js/bevolnuteer-custom.js
+++ public/js/bevolnuteer-custom.js
@@ -167,22 +167,22 @@
 //=============================================================
 // goole analytic
 //=============================================================
-	if (document.location.hostname.search("voluntariossf.org.ar") !== -1) {
-		(function(i, s, o, g, r, a, m) {
-			i['GoogleAnalyticsObject'] = r;
-			i[r] = i[r] || function() {
-				(i[r].q = i[r].q || []).push(arguments)
-			}, i[r].l = 1 * new Date();
-			a = s.createElement(o),
-				m = s.getElementsByTagName(o)[0];
-			a.async = 1;
-			a.src = g;
-			m.parentNode.insertBefore(a, m)
-		})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+	// if (document.location.hostname.search("voluntariossf.org.ar") !== -1) {
+	// 	(function(i, s, o, g, r, a, m) {
+	// 		i['GoogleAnalyticsObject'] = r;
+	// 		i[r] = i[r] || function() {
+	// 			(i[r].q = i[r].q || []).push(arguments)
+	// 		}, i[r].l = 1 * new Date();
+	// 		a = s.createElement(o),
+	// 			m = s.getElementsByTagName(o)[0];
+	// 		a.async = 1;
+	// 		a.src = g;
+	// 		m.parentNode.insertBefore(a, m)
+	// 	})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-		ga('create', 'UA-57007617-1', 'auto');
-		ga('send', 'pageview');
-	}
+	// 	ga('create', 'UA-57007617-1', 'auto');
+	// 	ga('send', 'pageview');
+	// }
 
 
 
